d1a01c2fc33e48f9d18b51331808c100e9fecedb
InTron <zy19970@hotmail.com>
2023-08-27 12:09:29 +0000

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,6 +43,8 @@ function disonload(){
 
   if (window.screen.width < 500)
   {
-    document.getElementById("socialsologn").style.display='';
+    document.getElementById("socialsologn1").style.display='';
+    document.getElementById("socialsologn2").style.display='';
+    document.getElementById("socialsologn3").style.display='';
   }
 }
